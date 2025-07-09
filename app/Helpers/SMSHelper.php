c72ce35b9be8463610d5f73466cb8912469690dc
Andrzej Kierepka <andrzejkierepka@gmail.com>
2025-07-09 19:27:29 +0200

    Poprawka sms

--- app/Helpers/SMSHelper.php
+++ app/Helpers/SMSHelper.php
@@ -5,7 +5,7 @@ namespace App\Helpers;
 class SMSHelper
 {
     public function sendSMS($phone, $message){
-        exec('gammu-smsd-inject TEXT $phone -unicode -text "$message"');
+        exec('gammu-smsd-inject TEXT '.$phone.' -unicode -text \"'.$message.'\"');
     }
 
 }
