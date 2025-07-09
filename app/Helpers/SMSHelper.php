a228550408abead77e5f6885d7bc0aa597cbfca6
Andrzej Kierepka <andrzejkierepka@gmail.com>
2025-07-09 19:43:40 +0200

    Unicode sms

--- app/Helpers/SMSHelper.php
+++ app/Helpers/SMSHelper.php
@@ -5,10 +5,9 @@ namespace App\Helpers;
 class SMSHelper
 {
     public function sendSMS($phone, $message){
-        $command = 'gammu-smsd-inject TEXT '.$phone.'  -text "'.$message.'"';
-        echo $command;
+        $command = 'gammu-smsd-inject TEXT '.$phone.'  -unicode -text "'.$message.'"';
         $data=exec($command);
-        echo var_dump($data);
+        echo $data;
     }
 
 }
